bca7057e0dda97abce75518000a9bfb42cc99150
Mona Lewin <m.lewin@student.fontys.nl>
2023-06-19 01:58:13 +0200

    improve error handling

--- shareplate-backend/src/controllers/push-notification.controller.ts
+++ shareplate-backend/src/controllers/push-notification.controller.ts
@@ -1,4 +1,4 @@
-import {post, requestBody} from '@loopback/rest';
+import {HttpErrors, post, requestBody} from '@loopback/rest';
 import {repository} from '@loopback/repository';
 import {inject} from '@loopback/core';
 import {
@@ -44,16 +44,16 @@ export class PushNotificationController {
       //Find the corresponding food offer based on the notification ID
       // The ID is the id of the user who should recieve the notifcation
       const foodOffer = await this.foodOfferController.findById(requestData.notification.id);
-      if(foodOffer.createdBy == null) {
-        throw new Error('Food offer not found for ID: $(requestData.notification.id}')
+      if(foodOffer == null) {
+        throw new HttpErrors.NotFound(`Food offer not found for ID: ${requestData.notification.id}`)
       }
       if(foodOffer.createdBy == null) {
-        throw new Error('CreatedBy is null for the food offer.')
+        throw new HttpErrors.NotFound('CreatedBy is null for the food offer.')
       }
       //Retrieve device token of the user who should recieve the notification
       const deviceToken = await this.userRepository.findDeviceTokenById(foodOffer.createdBy)
       if (deviceToken == null) {
-        throw new Error('Devie token not found for user ID: $(foodOffer.createdBy}')
+        throw new HttpErrors.NotFound(`Devie token not found for user ID: ${foodOffer.createdBy}`)
       }
       //Send the APN notification to the device token using the APN provider
       const result = await this.apnProvider.send(
